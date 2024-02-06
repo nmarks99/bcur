ccb0c4998e78769ff1ff7ad8d97034e583b5cf25
nmarks99 <nmarks@anl.gov>
2024-02-06 17:30:43 -0600

    add await_asyn_param function

--- iocBoot/iocbcur/scripts/bcur.lua
+++ iocBoot/iocbcur/scripts/bcur.lua
@@ -11,46 +11,51 @@ function get_bit(n, i)
 end
 
 
+-- waits for the 'asyn_param' on 'asyn_port' to
+-- equal the value of 'match'
+function await_asyn_param(asyn_port, asyn_param, match)
+    -- TODO: add timeout
+    if type(match) == "string" then
+        while true do
+            local val = asyn.readParam(asyn_port, 0, asyn_param) 
+            if val:find(match, 1, true) then
+                break
+            end
+        end
+    else
+        while true do
+            local val = asyn.readParam(asyn_port, 0, asyn_param)
+            if (val == match) then
+                break
+            end
+        end
+    end
+end
+
 local DASHBOARD_PORT = "asyn1"
 local RTDE_PORT = "asyn2"
 local ADDR = 0
 local MOVING_DOUT_BIT = 0 -- digital output bit robot moving flag
 
-local URP_PROGRAM_NAME = "blink.urp"
-
+local urp_program = "blink.urp"
 function basic_task()
-    -- TODO: timeouts
+    -- set robot as busy for duration of function
+    epics.put("bcur:Busy", 1)
 
-    -- load program, wait for it to be loaded
-    asyn.writeParam(DASHBOARD_PORT, ADDR, "LOAD_URP", URP_PROGRAM_NAME)
-    while true do
-        local val = asyn.readParam(DASHBOARD_PORT, ADDR, "LOADED_PROGRAM")
-        if val:find(URP_PROGRAM_NAME,1, true) then
-            print(string.format("%s", val))
-            break
-        end
-    end
+    -- load URP program and wait for it to be loaded
+    asyn.writeParam(DASHBOARD_PORT, ADDR, "LOAD_URP", urp_program)
+    await_asyn_param(DASHBOARD_PORT, "LOADED_PROGRAM", urp_program)
     print("program loaded!")
     
     -- play program, wait for it to start playing
     asyn.writeParam(DASHBOARD_PORT, ADDR, "PLAY", 1)
-    while true do
-        local val = asyn.readParam(RTDE_PORT, ADDR, "RUNTIME_STATE")
-        print(string.format("runtime state = %d", val))
-        if (val == 2) then
-            break
-        end
-    end
-    epics.put("bcur:Busy", 1)
+    await_asyn_param(RTDE_PORT, "RUNTIME_STATE", 2)
     print("program started!")
 
-    -- wait for program to be finished
-    while true do
-        local val = asyn.readParam(RTDE_PORT, ADDR, "RUNTIME_STATE")
-        if (val == 1) then
-            break
-        end
-    end
-    epics.put("bcur:Busy", 0)
+    -- wait for the program to be finished
+    await_asyn_param(RTDE_PORT, "RUNTIME_STATE", 1)
     print("Program finished!")
+    
+    -- robot is no longer busy
+    epics.put("bcur:Busy", 0)
 end
