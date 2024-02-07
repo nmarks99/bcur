26cc2c7ceb042e926dc274ca04ae51887d10de1f
nmarks99 <nmarks@anl.gov>
2024-02-07 11:03:40 -0600

    clean up bcur.lua, rename some stuff

--- iocBoot/iocbcur/scripts/bcur.lua
+++ iocBoot/iocbcur/scripts/bcur.lua
@@ -1,8 +1,13 @@
 asyn = require("asyn")
 epics = require("epics")
 
+-- must match asyn port names created in urRobot.cmd
+local DASHBOARD_PORT = "asyn_dash"
+local RTDE_PORT = "asyn_rtde"
+local ADDR = 0
+
 -- gets the i'th bit of the number n
-function get_bit(n, i)
+local function get_bit(n, i)
     if (n & (1 << i)) ~= 0 then
         return 1;
     else
@@ -11,49 +16,51 @@ function get_bit(n, i)
 end
 
 
--- waits for the 'asyn_param' on 'asyn_port' to
--- equal the value of 'match'
-function await_asyn_param(asyn_port, asyn_param, match)
-    -- TODO: add timeout
-    if type(match) == "string" then
-        while true do
-            local val = asyn.readParam(asyn_port, 0, asyn_param) 
-            if val:find(match, 1, true) then
-                break
-            end
+-- waits for the 'asyn_param' on 'asyn_port' to equal the value of 'match'
+-- if timeout is not specified, will not check for timeout
+local function wait_asyn_param(asyn_port, asyn_param, match, timeout)
+    local function string_match(val, match)
+        return val:find(match, 1, true)
+    end
+    local function num_match(val, match)
+        return val == match
+    end
+    local match_func = type(match) == "string" and string_match or num_match
+    
+    local t0 = os.time()
+    while true do
+        local val = asyn.readParam(asyn_port, 0, asyn_param)
+        if match_func(val, match) then
+            break
         end
-    else
-        while true do
-            local val = asyn.readParam(asyn_port, 0, asyn_param)
-            if (val == match) then
+        if timeout ~= nil then
+            local elap = (os.time() - t0)
+            if (elap >= timeout) then
+                print("timeout reached")
                 break
             end
         end
     end
 end
 
-local DASHBOARD_PORT = "asyn1"
-local RTDE_PORT = "asyn2"
-local ADDR = 0
-local MOVING_DOUT_BIT = 0 -- digital output bit robot moving flag
 
 local urp_program = "blink.urp"
-function basic_task()
+function blink_task()
     -- set robot as busy for duration of function
     epics.put("bcur:Busy", 1)
 
     -- load URP program and wait for it to be loaded
     asyn.writeParam(DASHBOARD_PORT, ADDR, "LOAD_URP", urp_program)
-    await_asyn_param(DASHBOARD_PORT, "LOADED_PROGRAM", urp_program)
+    wait_asyn_param(DASHBOARD_PORT, "LOADED_PROGRAM", urp_program)
     print("program loaded!")
     
     -- play program, wait for it to start playing
     asyn.writeParam(DASHBOARD_PORT, ADDR, "PLAY", 1)
-    await_asyn_param(RTDE_PORT, "RUNTIME_STATE", 2)
+    wait_asyn_param(RTDE_PORT, "RUNTIME_STATE", 2)
     print("program started!")
 
     -- wait for the program to be finished
-    await_asyn_param(RTDE_PORT, "RUNTIME_STATE", 1)
+    wait_asyn_param(RTDE_PORT, "RUNTIME_STATE", 1)
     print("Program finished!")
     
     -- robot is no longer busy
--- iocBoot/iocbcur/urRobot.lua
+++ iocBoot/iocbcur/urRobot.lua
@@ -1,12 +1,13 @@
 epicsEnvSet("ROBOT_NAME", "")
+epicsEnvSet("ROBOT_IP", "164.54.104.148")
 
 -- Set up UR dashboard server
-epicsEnvSet("UR_DASH_PORT", "asyn1")
+epicsEnvSet("UR_DASH_PORT", "asyn_dash")
 dbLoadRecords("$(URROBOT)/db/dashboard.db", "P=$(PREFIX), R=$(ROBOT_NAME), PORT=$(UR_DASH_PORT), ADDR=0")
-URRobotDashboardConfig("$(UR_DASH_PORT)", "164.54.104.148")
+URRobotDashboardConfig("$(UR_DASH_PORT)", "$(ROBOT_IP)")
 
 -- Set up UR RTDE interface
-epicsEnvSet("UR_RTDE_PORT", "asyn2")
+epicsEnvSet("UR_RTDE_PORT", "asyn_rtde")
 dbLoadRecords("$(URROBOT)/db/rtde_receive.db", "P=$(PREFIX), R=$(ROBOT_NAME), PORT=$(UR_RTDE_PORT), ADDR=0")
 dbLoadRecords("$(URROBOT)/db/rtde_io.db", "P=$(PREFIX), R=$(ROBOT_NAME), PORT=$(UR_RTDE_PORT), ADDR=0")
-URRobotRTDEConfig("$(UR_RTDE_PORT)", "164.54.104.148")
+URRobotRTDEConfig("$(UR_RTDE_PORT)", "$(ROBOT_IP)")
